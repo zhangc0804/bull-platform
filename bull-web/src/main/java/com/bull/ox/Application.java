d476af35da95d154acd6e1f5ff499b706ac5667c
zhangc0804 <zhangc0804@163.com>
2018-07-05 15:15:04 +0800

    修改项目结构：bull-web模块只包含web资源和启动程序

--- bull-web/src/main/java/com/bull/ox/Application.java
+++ bull-web/src/main/java/com/bull/ox/Application.java
@@ -1,15 +1,9 @@
 package com.bull.ox;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication()
-//@Configuration
-//@EnableAutoConfiguration
-//@ComponentScan
 public class Application {
 
     public static void main(String[] args) {
