bf19d94c9233056aafdcfb81d85f4702083a762a
zhangc0804 <zhangc0804@163.com>
2018-07-13 15:52:31 +0800

    单模块应用

--- src/main/java/com/bull/ox/config/ShiroConfig.java
+++ src/main/java/com/bull/ox/config/ShiroConfig.java
@@ -1,8 +1,26 @@
 package com.bull.ox.config;
 
+import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
+import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ShiroConfig {
+
+    @Bean
+    public ShiroFilterChainDefinition shiroFilterChainDefinition() {
+        DefaultShiroFilterChainDefinition chainDefinition = new DefaultShiroFilterChainDefinition();
+
+        // logged in users with the 'admin' role
+//        chainDefinition.addPathDefinition("/admin/**", "authc, roles[admin]");
+
+        // logged in users with the 'document:read' permission
+//        chainDefinition.addPathDefinition("/docs/**", "authc, perms[document:read]");
+
+        // all other paths require a logged in user
+//        chainDefinition.addPathDefinition("/**", "authc");
+        return chainDefinition;
+    }
 
 }
