cb2ff33074a3f0d9bab587c121b0f1bf0478f227
zhangc0804 <zhangc0804@163.com>
2018-07-13 17:19:44 +0800

    shiro添加配置：authorizer

--- src/main/java/com/bull/ox/config/ShiroConfig.java
+++ src/main/java/com/bull/ox/config/ShiroConfig.java
@@ -1,12 +1,30 @@
 package com.bull.ox.config;
 
+import com.bull.ox.sys.security.realm.SysAuthorizingRealm;
+import org.apache.shiro.authz.Authorizer;
+import org.apache.shiro.realm.Realm;
+import org.apache.shiro.realm.text.TextConfigurationRealm;
 import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
 import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ShiroConfig {
+
+    @Autowired
+    private SysAuthorizingRealm sysAuthorizingRealm;
+
+    @Bean
+    public Realm realm(){
+        return sysAuthorizingRealm;
+    }
+
+    @Bean
+    public Authorizer authorizer(){
+        return sysAuthorizingRealm;
+    }
 
     @Bean
     public ShiroFilterChainDefinition shiroFilterChainDefinition() {
