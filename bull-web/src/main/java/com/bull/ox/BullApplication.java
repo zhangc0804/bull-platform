48e3a82552be8c19629bcd52a2572b04234adc94
zhangc0804 <zhangc0804@163.com>
2018-08-08 16:38:23 +0800

    添加Springboot启动类

---
+++ bull-web/src/main/java/com/bull/ox/BullApplication.java
@@ -0,0 +1,11 @@
+package com.bull.ox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BullApplication.class,args);
+    }
+}
